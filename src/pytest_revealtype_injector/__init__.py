f6a014749dfb65eeb4dd71142d7d0a925fed15f2
Abel Cheung <abelcheung@gmail.com>
2024-11-27 19:40:45 +0000

    chore: Fix package description, flit only picks the first line as content

--- src/pytest_revealtype_injector/__init__.py
+++ src/pytest_revealtype_injector/__init__.py
@@ -1,5 +1,3 @@
-"""Pytest plugin for replacing reveal_type() calls inside test functions with
-static and runtime type checking result comparison, in order to confirm
-correctness of type annotation."""
+"""Pytest plugin for replacing reveal_type() calls inside test functions with static and runtime type checking result comparison, in order to confirm correctness of type annotation."""  # noqa: E501
 
 __version__ = "0.0.0"
