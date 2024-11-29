c55bf75d106c0ef9e2663c23631f01559e40a745
Phlynka <53543700+phlynka@users.noreply.github.com>
2024-11-30 00:22:22 +0800

    build: Change build backend to hatchling

--- src/pytest_revealtype_injector/__init__.py
+++ src/pytest_revealtype_injector/__init__.py
@@ -1,3 +1,3 @@
-"""Pytest plugin for replacing reveal_type() calls inside test functions with static and runtime type checking result comparison, in order to confirm correctness of type annotation."""  # noqa: E501
+"""Pytest plugin for replacing reveal_type() calls inside test functions with static and runtime type checking result comparison, for confirming type annotation validity."""  # noqa: E501
 
 __version__ = "0.0.0"
